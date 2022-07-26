5af8f15955e7368eb25ecef0f79334a2fac00f26
yashbhatt1304 <yashbhatt1304@gmail.com>
2022-07-26 16:39:02 +0530

    Added useState hook for handling user inputs.

--- src/components/Bootstrap/CreateOrder.js
+++ src/components/Bootstrap/CreateOrder.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../stylesheet.css'
 
 const CreateOrder = (props) => {
+  const [Id, setId]=useState('1234')
+  const [Name, setName]=useState('John Vinskey')
+  const [Address, setAddress]=useState('3/12 Kings Street, Manchester')
+
+  const handleId = (event) => {
+    event.preventDefault();
+    setId(event.target.value);
+  }
+  const handleName = (event) => {
+    event.preventDefault();
+    setName(event.target.value);
+  }
+  const handleAddress = (event) => {
+    event.preventDefault();
+    setAddress(event.target.value);
+  }
+  const handleClick = (event) => {
+    event.preventDefault();
+    console.log("Customer Id:"+Id);
+    console.log("Customer Name:"+Name);
+    console.log("Address:"+Address);
+  }
+
   return (
     <form>
       <div className='container'>
@@ -10,13 +33,24 @@ const CreateOrder = (props) => {
       </div>
       <div className='container'>
         <table className='new-order'>
-            <tr><td>Customer Id*</td><td><input type='text' name='Id' /></td></tr>
-            <tr><td>Customer Name*</td><td><input type='text' name='Id' /></td></tr>
-            <tr><td>Address</td><td><input type='text' name='Id' /></td></tr>
-        </table>
+          <tbody>
+            <tr>
+              <td>Customer Id*</td>
+              <td><input className='user-info' type='text' value={Id} onChange={handleId}/></td>
+            </tr>
+            <tr>
+              <td>Customer Name*</td>
+              <td><input className='user-info' type='text' value={Name} onChange={handleName}/></td>
+            </tr>
+            <tr>
+              <td>Address</td>
+              <td><input className='user-info' type='text' value={Address} onChange={handleAddress}/></td>
+            </tr>
+          </tbody>
+        </table> 
       </div>
       <div align='center'>
-        <button className='submit' type='submit'>Submit</button>
+        <button className='submit' type='submit' onClick={handleClick}>Submit</button>
       </div>
     </form>
   )
--- src/components/Bootstrap/GetOrderDetails.js
+++ src/components/Bootstrap/GetOrderDetails.js
@@ -1,6 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import '../stylesheet.css'
 
 const GetOrderDetails = (props) => {
+  const [Id, setId] = useState('1234');
+  const [Name, setName] = useState('John Vensky')
+  const [Number, setNumber] = useState('0123456789')
+
+  const handleId = (event) => {
+    event.preventDefault();
+    setId(event.target.value);
+  }
+  const handleName = (event) => {
+    event.preventDefault();
+    setName(event.target.value);
+  }
+  const handleNumber = (event) => {
+    event.preventDefault();
+    setNumber(event.target.value);
+  }
+  const handleClick = (event) => {
+    event.preventDefault();
+    console.log("Customer Id:"+Id);
+    console.log("Customer Name:"+Name);
+    console.log("Address:"+Number);
+  }
 
   return (
     <form>
@@ -10,13 +33,24 @@ const GetOrderDetails = (props) => {
       </div>
       <div className='container'>
         <table className='new-order'>
-            <tr><td>Customer Id*</td><td><input type='text' name='Id' /></td></tr>
-            <tr><td>Customer Name*</td><td><input type='text' name='Id' /></td></tr>
-            <tr><td>Order Number*</td><td><input type='text' name='Id' /></td></tr>
+          <tbody>
+            <tr>
+              <td>Customer Id*</td>
+              <td><input className='user-info' type='text' name='Id' value={Id} onChange={handleId}/></td>
+            </tr>
+            <tr>
+              <td>Customer Name*</td>
+              <td><input className='user-info' type='text' name='Id' value={Name} onChange={handleName}/></td>
+            </tr>
+            <tr>
+              <td>Order Number*</td>
+              <td><input className='user-info' type='text' name='Id' value={Number} onChange={handleNumber}/></td>
+            </tr>
+          </tbody>
         </table>
       </div>
       <div align='center'>
-        <button className='submit' type='submit'>Submit</button>
+        <button className='submit' type='submit' onClick={handleClick}>Submit</button>
       </div>
     </form>
   )
