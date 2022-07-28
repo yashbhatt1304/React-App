0839492da3f2760d5a2b527ebf1b52d6706566b0
yashbhatt1304 <yashbhatt1304@gmail.com>
2022-07-28 18:05:56 +0530

    Added loading functionality while fetching/updating data in backend

--- src/components/Bootstrap/CreateOrder.js
+++ src/components/Bootstrap/CreateOrder.js
@@ -6,7 +6,9 @@ const CreateOrder = (props) => {
   const [Id, setId]=useState('1234')
   const [Name, setName]=useState('John Vinskey')
   const [Address, setAddress]=useState('3/12 Kings Street, Manchester')
-
+  const [Bool, setBool] = useState(null)
+  const [Loading, setLoading] = useState(false)
+  
   window.order= {
     createdAt: '',
     customerId: Id,
@@ -14,23 +16,32 @@ const CreateOrder = (props) => {
     address: Address,
     pinCode: ''
    }
-   
+
   const handleId = (event) => {
     event.preventDefault();
+    setLoading(false);
+    setBool(null);
     setId(event.target.value);
   }
   const handleName = (event) => {
     event.preventDefault();
+    setLoading(false);
+    setBool(null);
     setName(event.target.value);
   }
   const handleAddress = (event) => {
     event.preventDefault();
+    setLoading(false);
+    setBool(null);
     setAddress(event.target.value);
   }
+
   async function handleClick(event) {
     event.preventDefault();
+    setLoading(true);
     const json = await ordersApi.postOrder(window.order);
     console.log(json);
+    setBool(json);
   }
 
   return (
@@ -60,6 +71,31 @@ const CreateOrder = (props) => {
       <div align='center'>
         <button className='submit' type='submit' onClick={handleClick}>Submit</button>
       </div>
+      {(Bool)?(
+        <div className='container-fluid' align='center'>
+        <p className='after-submit'>Order with below details is created</p>
+        <table className='table table-bordered'>
+          <thead>
+            <tr>
+              <th>Order Number</th>
+              <th>Customer Number</th>
+              <th>Customer Name</th>
+              <th>Address</th>
+              <th>Zip Code</th>
+              <th>Place</th>
+            </tr>
+            <tr>
+              <td>{Bool.orderId}</td>
+              <td>{Bool.customerId}</td>
+              <td>{Bool.customerName}</td>
+              <td>{Bool.address}</td>
+              <td>{Bool.pinCode}</td>
+              <td>{Bool.createdAt}</td>
+            </tr>
+          </thead>
+        </table>
+      </div>):
+      ((Loading)?(<div align="center"><p className='after-submit'>Uploading details</p></div>):'')}
     </form>
   )
 }
